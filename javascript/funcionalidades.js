2f7e38344eafd943b0df78c10e745e2b34061c9a
Manuel Acosta <76167771+manunolo@users.noreply.github.com>
2024-09-10 20:10:15 -0300

    Add files via upload

---
+++ javascript/funcionalidades.js
@@ -0,0 +1,32 @@
+let productos = [
+    {
+        titulo: "Pancho vegano",
+        precio : 0,
+        imagen : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWTsM9_KKdaryNYbqgb0kmKWpRpVr3na-Niw&s"
+    },
+    {
+        titulo: "Pancho Carnivoro",
+        precio : 1000,
+        imagen : "https://palermotour.com.ar/tourdenoticias/wp-content/uploads/2021/05/HotDog12345677HOME12.jpg"
+    },
+    {
+        titulo: "Pancho detonado",
+        precio : 1500,
+        imagen : "https://1.bp.blogspot.com/-i3U-P385p1s/YJcIQGxKbnI/AAAAAAAANzw/SEYWo3ufWJUHkyLl4hCQDO7H11wBM-RMACLcBGAsYHQ/s496/HotDog12.jpg"
+    },
+    {
+        titulo: "Pancho piolon",
+        precio : 2000,
+        imagen : "https://www.negociosypymes.com/wp-content/uploads/2021/09/pan-pancho-doble-760x400.png"
+    },
+    {
+        titulo: "Big pancho",
+        precio : 4000,
+        imagen : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe6hnWljkDEoXOuCGWabHX2WfBvr8VTBGQEw&s"
+    }
+];
+const newDiv = document.createElement("div");
+
+const newContent = document.createTextNode("Hi there and greetings!");
+
+newDiv.appendChild(newContent);
